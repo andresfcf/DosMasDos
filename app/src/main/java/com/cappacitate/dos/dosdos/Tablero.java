c8f324dd33b0ba9ccccf0c1a2cd9c1f096a0caf3
edwinValero <edwwin527@gmail.com>
2015-02-21 10:49:21 -0500

    Primer commit

---
+++ app/src/main/java/com/cappacitate/dos/dosdos/Tablero.java
@@ -0,0 +1,19 @@
+package com.cappacitate.dos.dosdos;
+
+import java.security.PublicKey;
+
+/**
+ * Created by kida on 21/02/2015.
+ */
+public class Tablero {
+
+    casillasDTO [][] tablero;
+
+    public Tablero(){
+
+        tablero = new casillasDTO[4][4];
+
+
+    }
+
+}
---
+++ app/src/main/java/com/cappacitate/dos/dosdos/casillasDTO.java
@@ -0,0 +1,20 @@
+package com.cappacitate.dos.dosdos;
+
+/**
+ * Created by kida on 21/02/2015.
+ */
+public class casillasDTO {
+
+    public int posX;
+    public int posY;
+    public int numero;
+
+
+   public casillasDTO(int posX, int posY, int numero){
+       this.posX=posX;
+       this.posY=posY;
+       this.numero=numero;
+
+    }
+
+}
